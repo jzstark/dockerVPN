2fd9f8c5f51e67b5c159110b7b71d732770b7edb
J.Roger Zhao <rho.ajax@gmail.com>
2016-07-31 20:32:20 +0000

    improved l2tp and rpi-nginx

--- Docker-rpi.sh
+++ Docker-rpi.sh
@@ -2,9 +2,9 @@
 
 sudo apt-get install -y apt-transport-https
 wget -q https://packagecloud.io/gpg.key -O - | sudo apt-key add -
-echo 'deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ wheezy main' | sudo tee /etc/apt/sources.list.d/hypriot.list
+echo 'deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ jessie main' | sudo tee /etc/apt/sources.list.d/hypriot.list
 sudo apt-get update
-sudo apt-get install -y docker-hypriot
+sudo apt-get install -y docker-hypriot docker-compose
 sudo systemctl enable docker
 
 # Add user to docker group
--- nginx/installNginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DIR=/home/Documents/dockerVPN
-
-docker run --name docker-nginx -p 80:80 -v $DIR/nginx/html:/usr/share/nginx/html -v $DIR/nginx/default.conf:/etc/nginx/conf.d/default.conf -d nginx
-
