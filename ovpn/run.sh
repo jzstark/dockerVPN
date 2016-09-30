3fb3f3538848ee1396a4db85c4931e880b6760a1
J.Roger Zhao <rho.ajax@gmail.com>
2016-09-30 18:32:41 +0100

    update ovpn

--- ovpn/run.sh
+++ ovpn/run.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+# Set up Certificate Store
+OVPN_DATA="ovpn-data"
+IFACE=wlan0
+docker run --name $OVPN_DATA -v /etc/openvpn hypriot/armhf-busybox
+
+# IP=`/sbin/ifconfig $IFACE | awk '/inet addr/{print substr($2,6)}'`
+IP=`curl --silent ipecho.net/plain`
+docker run --volumes-from $OVPN_DATA --rm matrixanger/rpi-ovpn ovpn_genconfig -u udp://$IP
+docker run --volumes-from $OVPN_DATA --rm -it matrixanger/rpi-ovpn ovpn_initpki
+
 # Run OpenVPN
 OVPN_DATA=ovpn-data
 docker run --name ovpn --volumes-from $OVPN_DATA -d -p 1194:1194/udp --cap-add=NET_ADMIN matrixanger/rpi-ovpn
